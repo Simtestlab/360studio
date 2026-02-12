0f9bfba5cfecd680ad91fb7e228938c3ad9a19b0
nallasivam_s <nallasivamselvaraj@gmail.com>
2026-02-12 20:18:48 +0530

    init commit

---
+++ 360digital.in/tabs/js/script.js
@@ -0,0 +1,90 @@
+<!-- sortable recipe & popup -->	
+$(window).load(function(){
+	$("a[data-fancybox-group=mostratedgallery]").fancybox({
+				'transitionIn'		: 'elastic',
+				'transitionOut'		: 'elastic',
+				'titlePosition' 	: 'over',
+				'titleFormat'		: function(title, currentArray, currentIndex, currentOpts) {
+					return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
+				}
+			})
+ 
+$("a[data-fancybox-group=recipegallery]").fancybox({
+				'transitionIn'		: 'elastic',
+				'transitionOut'		: 'elastic',
+				'titlePosition' 	: 'over',
+				'titleFormat'		: function(title, currentArray, currentIndex, currentOpts) {
+					return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
+				}
+			}).each(function() {		
+			
+			$(this).append('<span class="view">&nbsp;</span>'); 
+			
+		});;
+		
+			$("a[data-fancybox-group=bloggallery]").fancybox({
+				'transitionIn'		: 'elastic',
+				'transitionOut'		: 'none',
+				'titlePosition' 	: 'over',
+				'titleFormat'		: function(title, currentArray, currentIndex, currentOpts) {
+					return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
+					
+					
+				}
+			}).each(function() {		
+			
+			$(this).append('<span class="view">&nbsp;</span>'); 
+			
+		});;
+      
+	  $("a.fancyboxpopup").fancybox().each(function() {		
+			
+			$(this).append('<span class="view">&nbsp;</span>'); 
+			
+		});
+		
+		
+      var $container = $('.recipesortable');
+
+      $container.isotope({
+        itemSelector : '.element'
+      });
+      
+      
+      var $optionSets = $('#options .option-set'),
+          $optionLinks = $optionSets.find('a');
+
+      $optionLinks.click(function(){
+        var $this = $(this);
+        // don't proceed if already selected
+        if ( $this.hasClass('selected') ) {
+          return false;
+        }
+        var $optionSet = $this.parents('.option-set');
+        $optionSet.find('.selected').removeClass('selected');
+        $this.addClass('selected');
+  
+        // make option object dynamically, i.e. { filter: '.my-filter-class' }
+        var options = {},
+            key = $optionSet.attr('data-option-key'),
+            value = $this.attr('data-option-value');
+        // parse 'false' as false boolean
+        value = value === 'false' ? false : value;
+        options[ key ] = value;
+        if ( key === 'layoutMode' && typeof changeLayoutMode === 'function' ) {
+          // changes in layout modes need extra logic
+          changeLayoutMode( $this, options )
+        } else {
+          // otherwise, apply new options
+          $container.isotope( options );
+        }
+        
+        return false;
+      });
+
+		
+		
+    
+
+});
+
